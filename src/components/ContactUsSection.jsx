283c3095a06b3878fe8df20f1e67dfe6d1665113
Thando Somacele <thando.somacele@gmail.com>
2024-09-17 10:50:45 +0200

    change Founder Email icon

--- src/components/ContactUsSection.jsx
+++ src/components/ContactUsSection.jsx
@@ -98,7 +98,7 @@ const ContactUsSection = () => {
               <dl>
                 <div className='bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
                   <dt className='text-sm font-medium text-gray-500 flex items-center'>
-                    <Phone className='h-5 w-5 mr-2' /> Founder Email
+                    <Mail className='h-5 w-5 mr-2' /> Founder Email
                   </dt>
                   <dd className='mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'>
                     <a href='mailto:thando@legalscriber.co.za'>thando@legalscriber.co.za</a>
